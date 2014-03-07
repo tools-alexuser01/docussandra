57eedab73da1adaf3ecb7ef2131b1ee14a7e6062
Todd Fredrich <tfredrich@gmail.com>
2014-03-07 16:47:35 -0700

    Removed ByteBufferSerializer

--- src/main/java/com/strategicgains/mongossandra/serialization/JsonSerializationProcessor.java
+++ src/main/java/com/strategicgains/mongossandra/serialization/JsonSerializationProcessor.java
@@ -1,6 +1,5 @@
 package com.strategicgains.mongossandra.serialization;
 
-import java.nio.ByteBuffer;
 import java.util.UUID;
 
 import org.restexpress.serialization.json.JacksonJsonProcessor;
@@ -16,6 +15,5 @@ extends JacksonJsonProcessor
 	    super.initializeModule(module);
 	    module.addDeserializer(UUID.class, new UuidDeserializer());
 	    module.addSerializer(UUID.class, new UuidSerializer());
-	    module.addSerializer(ByteBuffer.class, new ByteBufferSerializer());
     }
 }
