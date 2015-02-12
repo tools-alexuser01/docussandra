8526ea33e6cc0d50c1f80681b39bb5752bb0708c
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-12 12:18:40 -0700

    IndexChangeObserver is now boilerplated

--- driver/src/main/java/com/strategicgains/docussandra/persistence/IndexChangeObserver.java
+++ driver/src/main/java/com/strategicgains/docussandra/persistence/IndexChangeObserver.java
@@ -1,7 +1,5 @@
 package com.strategicgains.docussandra.persistence;
 
-import com.strategicgains.docussandra.event.EventFactory;
-import com.strategicgains.eventing.DomainEvents;
 import com.strategicgains.repoexpress.domain.Identifiable;
 import com.strategicgains.repoexpress.event.AbstractRepositoryObserver;
 
@@ -12,39 +10,25 @@ import com.strategicgains.repoexpress.event.AbstractRepositoryObserver;
 public class IndexChangeObserver<T extends Identifiable>
         extends AbstractRepositoryObserver<T> {
 
-    private EventFactory<T> factory;
-
-    public IndexChangeObserver(EventFactory<T> eventFactory) {
+    public IndexChangeObserver() {
         super();
-        this.factory = eventFactory;
     }
 
     @Override
     public void afterCreate(T object) {
-        if (factory != null) {
-            publish(factory.newCreatedEvent(object));
-        }
+        //create the iTable
     }
 
     @Override
     public void afterDelete(T object) {
-        if (factory != null) {
-            publish(factory.newDeletedEvent(object));
-        }
+        //drop the iTable
     }
 
     @Override
     public void afterUpdate(T object) {
-        if (factory != null) {
-            publish(factory.newUpdatedEvent(object));
-        }
-    }
-
-    private void publish(Object event) {
-        if (event == null) {
-            return;
-        }
-
-        DomainEvents.publish(event);
+        //TODO: schedule job to handle this at a later time (off hours); very expensive operation
+        //drop the iTable
+        //recreate the iTable
+        //repopulate the iTable with existing data
     }
 }
