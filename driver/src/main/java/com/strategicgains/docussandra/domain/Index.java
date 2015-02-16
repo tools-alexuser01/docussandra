389e228d0b018740c932ab1e08c653d5278d1b20
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-16 09:48:58 -0700

    code format only

--- driver/src/main/java/com/strategicgains/docussandra/domain/Index.java
+++ driver/src/main/java/com/strategicgains/docussandra/domain/Index.java
@@ -16,221 +16,186 @@ import com.strategicgains.syntaxe.annotation.Required;
 import java.util.Objects;
 
 public class Index
-extends AbstractTimestampedIdentifiable
-implements Validatable
-{
+        extends AbstractTimestampedIdentifiable
+        implements Validatable {
 
-	@Required("Table")
-	@ChildValidation
-	private TableReference table;
+    @Required("Table")
+    @ChildValidation
+    private TableReference table;
 
-	@RegexValidation(name = "Index name", nullable = false, pattern = Constants.NAME_PATTERN, message = Constants.NAME_MESSAGE)
-	private String name;
+    @RegexValidation(name = "Index name", nullable = false, pattern = Constants.NAME_PATTERN, message = Constants.NAME_MESSAGE)
+    private String name;
 
-	private boolean isUnique = false;
+    private boolean isUnique = false;
 
-	/**
-	 * This is how many items will be stored in a single wide row, before creating another wide row.
-	 */
-	private long bucketSize = 2000l;
+    /**
+     * This is how many items will be stored in a single wide row, before
+     * creating another wide row.
+     */
+    private long bucketSize = 2000l;
 
-	/**
-	 * The list of fields, in order, that are being indexed. Prefixing a field with a dash ('-') means
-	 * it's order in descending order.
-	 */
-	@Required("Fields")
-	private List<String> fields;
+    /**
+     * The list of fields, in order, that are being indexed. Prefixing a field
+     * with a dash ('-') means it's order in descending order.
+     */
+    @Required("Fields")
+    private List<String> fields;
 
 //	@Required("Index Type")
 //	private IndexType type;
+    /**
+     * Consider the index is only concerned with only a partial dataset. In this
+     * case, instead of storing the entire BSON payload, we store only a
+     * subset--those listed in includeOnly.
+     */
+    private List<String> includeOnly;
 
-	/**
-	 * Consider the index is only concerned with only a partial dataset. In this case, instead of storing the entire BSON
-	 * payload, we store only a subset--those listed in includeOnly.
-	 */
-	private List<String> includeOnly;
-
-	public Index()
-	{
-	}
-
-	public Index(String name)
-	{
-		this();
-		name(name);
-	}
-
-	public boolean hasTable()
-	{
-		return (table != null);
-	}
-
-	public Table table()
-	{
-		return (hasTable() ? table.asObject() : null);
-	}
-
-	public void table(String database, String table)
-	{
-		this.table = new TableReference(database, table);
-	}
-
-	public void table(Table table)
-	{
-		this.table = (table != null ? new TableReference(table) : null);
-	}
-
-	public String tableName()
-	{
-		return (hasTable() ? table.name() : null);
-	}
-
-	public String databaseName()
-	{
-		return (hasTable() ? table.database() : null);
-	}
-
-	public String name()
-	{
-		return name;
-	}
-
-	public Index name(String name)
-	{
-		this.name = name;
-		return this;
-	}
-
-	public boolean isUnique()
-	{
-		return isUnique;
-	}
-
-	public Index isUnique(boolean value)
-	{
-		this.isUnique = value;
-		return this;
-	}
-
-	public void fields(List<String> props)
-	{
-		this.fields = new ArrayList<String>(props);
-	}
-
-	public List<String> fields()
-	{
-		return (fields == null ? Collections.<String>emptyList() : Collections.unmodifiableList(fields));
-	}
-
-	public void includeOnly(List<String> props)
-	{
-		this.includeOnly = new ArrayList<String>(props);
-	}
-
-	public List<String> includeOnly()
-	{
-		return (includeOnly == null ? Collections.<String>emptyList() : Collections.unmodifiableList(includeOnly));
-	}
-
-	@Override
-    public Identifier getId()
-    {
-		return new Identifier(databaseName(), tableName(), name);
+    public Index() {
     }
 
-	@Override
-    public void setId(Identifier id)
-    {
-		// intentionally left blank.
+    public Index(String name) {
+        this();
+        name(name);
     }
 
-	public long bucketSize()
-	{
-		return bucketSize;
-	}
+    public boolean hasTable() {
+        return (table != null);
+    }
 
-	public void bucketSize(long bucketSize)
-	{
-		this.bucketSize = bucketSize;
-	}
+    public Table table() {
+        return (hasTable() ? table.asObject() : null);
+    }
 
-	public void iterateFields(Callback<IndexField> callback)
-	{
-		for (String field : fields)
-		{
-			callback.process(new IndexField(field));
-		}
-	}
+    public void table(String database, String table) {
+        this.table = new TableReference(database, table);
+    }
 
-	public class IndexField
-	{
-		private String field;
-		private boolean isAscending = true;
+    public void table(Table table) {
+        this.table = (table != null ? new TableReference(table) : null);
+    }
 
-		public IndexField(String value)
-		{
-			field = value.trim();
+    public String tableName() {
+        return (hasTable() ? table.name() : null);
+    }
 
-			if (field.trim().startsWith("-"))
-			{
-				field = value.substring(1);
-				isAscending = false;
-			}
-		}
+    public String databaseName() {
+        return (hasTable() ? table.database() : null);
+    }
 
-		public String field()
-		{
-			return field;
-		}
+    public String name() {
+        return name;
+    }
 
-		public boolean isAscending()
-		{
-			return isAscending;
-		}
-	}
+    public Index name(String name) {
+        this.name = name;
+        return this;
+    }
 
-	@Override
-    public void validate()
-    {
-		final List<String> errors = new ArrayList<String>();
+    public boolean isUnique() {
+        return isUnique;
+    }
 
-		if (fields.isEmpty())
-		{
-			errors.add("Fields is required.");
-		}
+    public Index isUnique(boolean value) {
+        this.isUnique = value;
+        return this;
+    }
 
-		Pattern fieldPattern = Pattern.compile("^[\\+-]?\\w+");
+    public void fields(List<String> props) {
+        this.fields = new ArrayList<String>(props);
+    }
 
-		for (String field : fields)
-		{
-			if (!fieldPattern.matcher(field).matches())
-			{
-				errors.add("Invalid index field name: " + field);
-			}
-		}
+    public List<String> fields() {
+        return (fields == null ? Collections.<String>emptyList() : Collections.unmodifiableList(fields));
+    }
 
-		Pattern includePattern = Pattern.compile("^\\w+");
+    public void includeOnly(List<String> props) {
+        this.includeOnly = new ArrayList<String>(props);
+    }
 
-		if (includeOnly != null)
-		{
-			if (includeOnly.isEmpty())
-			{
-				errors.add("'includeOnly' cannot be empty, if included.");
-			}
+    public List<String> includeOnly() {
+        return (includeOnly == null ? Collections.<String>emptyList() : Collections.unmodifiableList(includeOnly));
+    }
 
-			for (String field : includeOnly)
-			{
-				if (!includePattern.matcher(field).matches())
-				{
-					errors.add("Invalid 'includeOnly' field name: " + field);
-				}
-			}
-		}
+    @Override
+    public Identifier getId() {
+        return new Identifier(databaseName(), tableName(), name);
+    }
 
-		if (!errors.isEmpty())
-		{
-			throw new ValidationException(errors);
-		}
+    @Override
+    public void setId(Identifier id) {
+        // intentionally left blank.
+    }
+
+    public long bucketSize() {
+        return bucketSize;
+    }
+
+    public void bucketSize(long bucketSize) {
+        this.bucketSize = bucketSize;
+    }
+
+    public void iterateFields(Callback<IndexField> callback) {
+        for (String field : fields) {
+            callback.process(new IndexField(field));
+        }
+    }
+
+    public class IndexField {
+
+        private String field;
+        private boolean isAscending = true;
+
+        public IndexField(String value) {
+            field = value.trim();
+
+            if (field.trim().startsWith("-")) {
+                field = value.substring(1);
+                isAscending = false;
+            }
+        }
+
+        public String field() {
+            return field;
+        }
+
+        public boolean isAscending() {
+            return isAscending;
+        }
+    }
+
+    @Override
+    public void validate() {
+        final List<String> errors = new ArrayList<String>();
+
+        if (fields.isEmpty()) {
+            errors.add("Fields is required.");
+        }
+
+        Pattern fieldPattern = Pattern.compile("^[\\+-]?\\w+");
+
+        for (String field : fields) {
+            if (!fieldPattern.matcher(field).matches()) {
+                errors.add("Invalid index field name: " + field);
+            }
+        }
+
+        Pattern includePattern = Pattern.compile("^\\w+");
+
+        if (includeOnly != null) {
+            if (includeOnly.isEmpty()) {
+                errors.add("'includeOnly' cannot be empty, if included.");
+            }
+
+            for (String field : includeOnly) {
+                if (!includePattern.matcher(field).matches()) {
+                    errors.add("Invalid 'includeOnly' field name: " + field);
+                }
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            throw new ValidationException(errors);
+        }
     }
 
     @Override
@@ -274,5 +239,5 @@ implements Validatable
         }
         return true;
     }
-    
+
 }
