aedd31aa0ba6a5de52991476fde5bc2f524a9cdb
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-16 10:24:12 -0700

    javadoc

--- driver/src/main/java/com/strategicgains/docussandra/persistence/ITableDao.java
+++ driver/src/main/java/com/strategicgains/docussandra/persistence/ITableDao.java
@@ -59,6 +59,11 @@ public class ITableDao {
         this.session = session;
     }
 
+    /**
+     * Checks to see if an iTable exists for the specified index.
+     * @param index Index that you want to check if it has a corresponding iTable.
+     * @return True if the iTable exists for the index, false otherwise.
+     */
     public boolean iTableExists(Index index) {
         logger.info("Checking for existance of iTable for index: " + index.toString());
         PreparedStatement createStmt = session.prepare(TABLE_EXISTENCE_CQL);
@@ -73,6 +78,10 @@ public class ITableDao {
         return false;
     }
 
+    /**
+     * Creates an iTable for the specified index.
+     * @param index Index that needs an iTable created for it.
+     */
     public void createITable(Index index) {
         logger.info("Creating iTable for index: " + index.toString());
         PreparedStatement createStmt = session.prepare(generateTableCreationSyntax(index));
@@ -80,6 +89,11 @@ public class ITableDao {
         session.execute(bs);
     }
 
+    /**
+     * Dynamically generates a table creation command for an iTable based on an index.
+     * @param index Index that needs an iTable generated for it.
+     * @return A CQL table creation command that will create the specified iTable.
+     */
     private String generateTableCreationSyntax(Index index) {
         String newTableName = Utils.calculateITableName(index);
         StringBuilder fieldCreateStatement = new StringBuilder();
@@ -102,11 +116,20 @@ public class ITableDao {
         return finalStatement;
     }
 
+    
+    /**
+     * Deletes an iTable
+     * @param index index whose iTable should be deleted
+     */
     public void deleteITable(Index index) {        
         String tableToDelete = Utils.calculateITableName(index);
         deleteITable(tableToDelete);
     }
 
+    /**
+     * Deletes an iTable
+     * @param tableName iTable name to delete.
+     */
     public void deleteITable(String tableName) {
         logger.info("Deleting iTable: " + tableName);
         String stmt = String.format(TABLE_DELETE_CQL, tableName);
