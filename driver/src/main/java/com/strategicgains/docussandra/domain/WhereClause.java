8495669f3835d1ee0641acdc64157097ef47a4ef
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-23 07:29:39 -0700

    work on whereclause object

--- driver/src/main/java/com/strategicgains/docussandra/domain/WhereClause.java
+++ driver/src/main/java/com/strategicgains/docussandra/domain/WhereClause.java
@@ -1,21 +1,33 @@
 package com.strategicgains.docussandra.domain;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+import org.restexpress.common.util.StringUtils;
 
 /**
- * Object that represents a CQL where clause to the extent that we need to process it.
+ * Object that represents a CQL where clause to the extent that we need to
+ * process it.
+ *
  * @author udeyoje
  */
 public class WhereClause {
-    
+
+    private static final int FIELD = 0;
+    private static final int OPERATOR = 1;
+    private static final int VALUE = 2;
+    private static final int CONJUNCTION = 3;
+    private static final int END = 4;
+
     private final String whereClause;
-    
+
+    private final String boundStatementSyntax = "";
+
     /**
-     * Field Names in a where clause. Corresponds to values. 
+     * Field Names in a where clause. Corresponds to values.
      */
     private ArrayList<String> fields = new ArrayList<>();
     /**
-     * Field Values in a where clause. Corresponds to fields. 
+     * Field Values in a where clause. Corresponds to fields.
      */
     private ArrayList<String> values = new ArrayList<>();
 
@@ -23,19 +35,44 @@ public class WhereClause {
         this.whereClause = whereClause;
         parse(whereClause);
     }
-    
-    private void parse(String whereClause){
-    
+
+    private void parse(String whereClause) {
+
+        StringBuilder boundStatementBuilder = new StringBuilder();
+        //foo = 'fooish' AND bar < 'barish'... [ORDER BY] foo
+        String[] tokens = whereClause.split("\\Q \\E");
+        int currentOperator = FIELD;
+        for (String token : tokens) {
+            switch (currentOperator) {
+                case FIELD:
+                    fields.add(token);
+                    boundStatementBuilder.append(token);
+                    break;
+                case OPERATOR:
+                    boundStatementBuilder.append(token);
+                    break;
+                case VALUE:
+                    boundStatementBuilder.append(token);
+                    break;
+                case CONJUNCTION:
+                    
+                    break;
+                case END:
+
+                    break;
+            }
+            boundStatementBuilder.append(" ");//tokens need to be re-space seperated
+        }
+
     }
-    
-    
-    
-    public String getBoundStatementSyntax(){
-        throw new UnsupportedOperationException("Not done yet");
+
+    public String getBoundStatementSyntax() {
+        return boundStatementSyntax;
     }
 
     /**
      * Field Names in a where clause. Corresponds to values.
+     *
      * @return the fields
      */
     public ArrayList<String> getFields() {
@@ -44,12 +81,11 @@ public class WhereClause {
 
     /**
      * Field Values in a where clause. Corresponds to fields.
+     *
      * @return the values
      */
     public ArrayList<String> getValues() {
         return values;
     }
-    
-    
-    
+
 }
