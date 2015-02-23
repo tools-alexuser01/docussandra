af89bfcb8c8984d64df4a3df2433b3d4f162e82c
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-23 16:06:32 -0700

    tests pass; CRUD with buckets work -- i don't know how we are going to do queries yet

--- driver/src/main/java/com/strategicgains/docussandra/persistence/DocumentRepository.java
+++ driver/src/main/java/com/strategicgains/docussandra/persistence/DocumentRepository.java
@@ -13,6 +13,8 @@ import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.mongodb.util.JSON;
+import com.strategicgains.docussandra.bucketmanagement.IndexBucketLocator;
+import com.strategicgains.docussandra.bucketmanagement.SimpleIndexBucketLocatorImpl;
 import com.strategicgains.docussandra.domain.Document;
 import com.strategicgains.docussandra.domain.Table;
 import com.strategicgains.docussandra.event.DocumentCreatedEvent;
@@ -29,257 +31,236 @@ import com.strategicgains.repoexpress.exception.InvalidObjectIdException;
 import com.strategicgains.repoexpress.exception.ItemNotFoundException;
 
 public class DocumentRepository
-extends AbstractObservableRepository<Document>
-{
-	private Session session;
+        extends AbstractObservableRepository<Document> {
 
-	private class Columns
-	{
-		static final String ID = "id";
-		static final String OBJECT = "object";
-		static final String CREATED_AT = "created_at";
-		static final String UPDATED_AT = "updated_at";
-	}
+    private Session session;
 
-	private static final String EXISTENCE_CQL = "select count(*) from %s where %s = ?";
-	private static final String READ_CQL = "select * from %s where %s = ?";
-	private static final String DELETE_CQL = "delete from %s where %s = ?";
-	private static final String UPDATE_CQL = "update %s set object = ?, updated_at = ? where %s = ?";
-	private static final String CREATE_CQL = "insert into %s (%s, object, created_at, updated_at) values (?, ?, ?, ?)";
+    private class Columns {
 
-	private Map<Table, PreparedStatement> createStmts = new HashMap<>();
-	private Map<Table, PreparedStatement> readStmts = new HashMap<>();
-	private Map<Table, PreparedStatement> existsStmts = new HashMap<>();
-	private Map<Table, PreparedStatement> updateStmts = new HashMap<>();
-	private Map<Table, PreparedStatement> deleteStmts = new HashMap<>();
-
-	public DocumentRepository(Session session)
-    {
-	    super();
-	    this.session = session;
-	    addObserver(new UuidIdentityRepositoryObserver<Document>());
-		addObserver(new DefaultTimestampedIdentifiableRepositoryObserver<Document>());
-		addObserver(new StateChangeEventingObserver<Document>(new DocumentEventFactory()));
+        static final String ID = "id";
+        static final String OBJECT = "object";
+        static final String CREATED_AT = "created_at";
+        static final String UPDATED_AT = "updated_at";
     }
 
-	protected Session session()
-    {
-    	return session;
+    private static final String EXISTENCE_CQL = "select count(*) from %s where %s = ?";
+    private static final String READ_CQL = "select * from %s where %s = ?";
+    private static final String DELETE_CQL = "delete from %s where %s = ?";
+    private static final String UPDATE_CQL = "update %s set object = ?, updated_at = ? where %s = ?";
+    private static final String CREATE_CQL = "insert into %s (%s, object, created_at, updated_at) values (?, ?, ?, ?)";
+
+    private Map<Table, PreparedStatement> createStmts = new HashMap<>();
+    private Map<Table, PreparedStatement> readStmts = new HashMap<>();
+    private Map<Table, PreparedStatement> existsStmts = new HashMap<>();
+    private Map<Table, PreparedStatement> updateStmts = new HashMap<>();
+    private Map<Table, PreparedStatement> deleteStmts = new HashMap<>();
+
+    private final IndexBucketLocator bucketLocator;
+
+    public DocumentRepository(Session session) {
+        super();
+        this.session = session;
+        this.bucketLocator = new SimpleIndexBucketLocatorImpl();
+        addObserver(new UuidIdentityRepositoryObserver<Document>());
+        addObserver(new DefaultTimestampedIdentifiableRepositoryObserver<Document>());
+        addObserver(new StateChangeEventingObserver<Document>(new DocumentEventFactory()));
     }
 
-	@Override
-	public Document doCreate(Document entity)
-	{
-		if (exists(entity.getId()))
-		{
-			throw new DuplicateItemException(entity.getClass().getSimpleName()
-			    + " ID already exists: " + entity.getId().toString());
-		}
+    protected Session session() {
+        return session;
+    }
 
-		Table table = entity.table();
-		PreparedStatement createStmt = createStmts.get(table);
+    @Override
+    public Document doCreate(Document entity) {
+        if (exists(entity.getId())) {
+            throw new DuplicateItemException(entity.getClass().getSimpleName()
+                    + " ID already exists: " + entity.getId().toString());
+        }
 
-		if (createStmt == null)
-		{
-			createStmt = session().prepare(String.format(CREATE_CQL, table.toDbTable(), Columns.ID));
-			createStmts.put(table, createStmt);//TODO: udeyoje: This seems dangrous (memory overrun); should we use a caching lib instead?
-		}
+        Table table = entity.table();
+        PreparedStatement createStmt = createStmts.get(table);
 
-		BoundStatement bs = new BoundStatement(createStmt);
-		bindCreate(bs, entity);
-		session().execute(bs);
-                List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentCreateIndexEntriesStatements(session, entity);
-                for(BoundStatement boundIndexStatement : indexStatements){
-                    session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
-                }
-		return entity;
-	}
+        if (createStmt == null) {
+            createStmt = session().prepare(String.format(CREATE_CQL, table.toDbTable(), Columns.ID));
+            createStmts.put(table, createStmt);//TODO: udeyoje: This seems dangrous (memory overrun); should we use a caching lib instead?
+        }
 
-	@Override
-	public Document doRead(Identifier identifier)
-	{
-		Table table = extractTable(identifier);
-		Identifier id = extractId(identifier);
-		PreparedStatement readStmt = readStmts.get(table);
+        BoundStatement bs = new BoundStatement(createStmt);
+        bindCreate(bs, entity);
+        session().execute(bs);
+        List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentCreateIndexEntriesStatements(session, entity, bucketLocator);
+        for (BoundStatement boundIndexStatement : indexStatements) {
+            session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
+        }
+        return entity;
+    }
 
-		if (readStmt == null)
-		{
-			readStmt = session().prepare(String.format(READ_CQL, table.toDbTable(), Columns.ID));
-			readStmts.put(table, readStmt);
-		}
-		
-		BoundStatement bs = new BoundStatement(readStmt);
-		bindIdentifier(bs, id);
-		Document item = marshalRow(session().execute(bs).one());
+    @Override
+    public Document doRead(Identifier identifier) {
+        Table table = extractTable(identifier);
+        Identifier id = extractId(identifier);
+        PreparedStatement readStmt = readStmts.get(table);
 
-		if (item == null)
-		{
-			throw new ItemNotFoundException("ID not found: " + identifier.toString());
-		}
+        if (readStmt == null) {
+            readStmt = session().prepare(String.format(READ_CQL, table.toDbTable(), Columns.ID));
+            readStmts.put(table, readStmt);
+        }
 
-		return item;
-	}
+        BoundStatement bs = new BoundStatement(readStmt);
+        bindIdentifier(bs, id);
+        Document item = marshalRow(session().execute(bs).one());
 
-	@Override
-	public Document doUpdate(Document entity)
-	{
-		if (!exists(entity.getId()))
-		{
-			throw new ItemNotFoundException(entity.getClass().getSimpleName()
-			    + " ID not found: " + entity.getId().toString());
-		}
+        if (item == null) {
+            throw new ItemNotFoundException("ID not found: " + identifier.toString());
+        }
 
-		Table table = entity.table();
-		PreparedStatement updateStmt = updateStmts.get(table);
+        return item;
+    }
 
-		if (updateStmt == null)
-		{
-			updateStmt = session().prepare(String.format(UPDATE_CQL, table.toDbTable(), Columns.ID));
-			updateStmts.put(table, updateStmt);
-		}
+    @Override
+    public Document doUpdate(Document entity) {
+        if (!exists(entity.getId())) {
+            throw new ItemNotFoundException(entity.getClass().getSimpleName()
+                    + " ID not found: " + entity.getId().toString());
+        }
 
-		BoundStatement bs = new BoundStatement(updateStmt);
-		bindUpdate(bs, entity);
-		session().execute(bs);
-                List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentUpdateIndexEntriesStatements(session, entity);
-                for(BoundStatement boundIndexStatement : indexStatements){
-                    session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
-                }
-		return entity;
-	}
+        Table table = entity.table();
+        PreparedStatement updateStmt = updateStmts.get(table);
 
-	@Override
-	public void doDelete(Document entity)
-	{
-		try
-		{
-			Table table = entity.table();
-			Identifier id = extractId(entity.getId());
-			PreparedStatement deleteStmt = deleteStmts.get(table);
+        if (updateStmt == null) {
+            updateStmt = session().prepare(String.format(UPDATE_CQL, table.toDbTable(), Columns.ID));
+            updateStmts.put(table, updateStmt);
+        }
 
-			if (deleteStmt == null)
-			{
-				deleteStmt = session().prepare(String.format(DELETE_CQL, table.toDbTable(), Columns.ID));
-				deleteStmts.put(table, deleteStmt);
-			}
+        BoundStatement bs = new BoundStatement(updateStmt);
+        bindUpdate(bs, entity);
+        session().execute(bs);
+        List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentUpdateIndexEntriesStatements(session, entity, bucketLocator);
+        for (BoundStatement boundIndexStatement : indexStatements) {
+            session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
+        }
+        return entity;
+    }
 
-			BoundStatement bs = new BoundStatement(deleteStmt);
-			bindIdentifier(bs, id);
-			session().execute(bs);
-                        List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentDeleteIndexEntriesStatements(session, entity);
-                        for(BoundStatement boundIndexStatement : indexStatements){
-                            session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
-                        }
-		}
-		catch (InvalidObjectIdException e)
-		{
-			throw new ItemNotFoundException("ID not found: " + entity.getId().toString());
-		}
-	}
+    @Override
+    public void doDelete(Document entity) {
+        try {
+            Table table = entity.table();
+            Identifier id = extractId(entity.getId());
+            PreparedStatement deleteStmt = deleteStmts.get(table);
 
-	@Override
-	public boolean exists(Identifier identifier)
-	{
-		if (identifier == null || identifier.isEmpty()) return false;
+            if (deleteStmt == null) {
+                deleteStmt = session().prepare(String.format(DELETE_CQL, table.toDbTable(), Columns.ID));
+                deleteStmts.put(table, deleteStmt);
+            }
 
-		Table table = extractTable(identifier);
-		Identifier id = extractId(identifier);
-		PreparedStatement existStmt = existsStmts.get(table);
+            BoundStatement bs = new BoundStatement(deleteStmt);
+            bindIdentifier(bs, id);
+            session().execute(bs);
+            List<BoundStatement> indexStatements = IndexMaintainerHelper.generateDocumentDeleteIndexEntriesStatements(session, entity, bucketLocator);
+            for (BoundStatement boundIndexStatement : indexStatements) {
+                session().execute(boundIndexStatement);//TODO: run in batch -- issue #36
+            }
+        } catch (InvalidObjectIdException e) {
+            throw new ItemNotFoundException("ID not found: " + entity.getId().toString());
+        }
+    }
 
-		if (existStmt == null)
-		{
-			existStmt = session().prepare(String.format(EXISTENCE_CQL, table.toDbTable(), Columns.ID));
-			existsStmts.put(table, existStmt);
-		}
+    @Override
+    public boolean exists(Identifier identifier) {
+        if (identifier == null || identifier.isEmpty()) {
+            return false;
+        }
 
-		BoundStatement bs = new BoundStatement(existStmt);
-		bindIdentifier(bs, id);
-		return (session().execute(bs).one().getLong(0) > 0);
-	}
+        Table table = extractTable(identifier);
+        Identifier id = extractId(identifier);
+        PreparedStatement existStmt = existsStmts.get(table);
 
-	private void bindIdentifier(BoundStatement bs, Identifier identifier)
-	{
-		bs.bind(identifier.primaryKey());
+        if (existStmt == null) {
+            existStmt = session().prepare(String.format(EXISTENCE_CQL, table.toDbTable(), Columns.ID));
+            existsStmts.put(table, existStmt);
+        }
+
+        BoundStatement bs = new BoundStatement(existStmt);
+        bindIdentifier(bs, id);
+        return (session().execute(bs).one().getLong(0) > 0);
+    }
+
+    private void bindIdentifier(BoundStatement bs, Identifier identifier) {
+        bs.bind(identifier.primaryKey());
 //		bs.bind(identifier.components().toArray());
-	}
+    }
 
-	private void bindCreate(BoundStatement bs, Document entity)
-	{
-		BSONObject bson = (BSONObject) JSON.parse(entity.object());
-		bs.bind(entity.getUuid(),
-			ByteBuffer.wrap(BSON.encode(bson)),
-		    entity.getCreatedAt(),
-		    entity.getUpdatedAt());
-	}
+    private void bindCreate(BoundStatement bs, Document entity) {
+        BSONObject bson = (BSONObject) JSON.parse(entity.object());
+        bs.bind(entity.getUuid(),
+                ByteBuffer.wrap(BSON.encode(bson)),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt());
+    }
 
-	private void bindUpdate(BoundStatement bs, Document entity)
-	{
-		BSONObject bson = (BSONObject) JSON.parse(entity.object());
+    private void bindUpdate(BoundStatement bs, Document entity) {
+        BSONObject bson = (BSONObject) JSON.parse(entity.object());
 
-		bs.bind(ByteBuffer.wrap(BSON.encode(bson)),
-			entity.getUpdatedAt(),
-			entity.getUuid());
-	}
+        bs.bind(ByteBuffer.wrap(BSON.encode(bson)),
+                entity.getUpdatedAt(),
+                entity.getUuid());
+    }
 
-	private Identifier extractId(Identifier identifier)
-    {
+    private Identifier extractId(Identifier identifier) {
 		// This includes the date/version on the end...
 //		List<Object> l = identifier.components().subList(2, 4);
 
-		//TODO: determine what to do with version here.
-		List<Object> l = identifier.components().subList(2, 3);
-		return new Identifier(l.toArray());
+        //TODO: determine what to do with version here.
+        List<Object> l = identifier.components().subList(2, 3);
+        return new Identifier(l.toArray());
     }
 
-	private Table extractTable(Identifier identifier)
-    {
-		Table t = new Table();
-		List<Object> l = identifier.components().subList(0, 2);
-		t.database((String) l.get(0));
-		t.name((String) l.get(1));
-		return t;
+    private Table extractTable(Identifier identifier) {
+        Table t = new Table();
+        List<Object> l = identifier.components().subList(0, 2);
+        t.database((String) l.get(0));
+        t.name((String) l.get(1));
+        return t;
     }
 
-    private Document marshalRow(Row row)
-    {
-		if (row == null) return null;
+    private Document marshalRow(Row row) {
+        if (row == null) {
+            return null;
+        }
 
-		Document d = new Document();
-		d.setUuid(row.getUUID(Columns.ID));
-		ByteBuffer b = row.getBytes(Columns.OBJECT);
+        Document d = new Document();
+        d.setUuid(row.getUUID(Columns.ID));
+        ByteBuffer b = row.getBytes(Columns.OBJECT);
 
-		if (b != null && b.hasArray())
-		{
-			byte[] result = new byte[b.remaining()];
-			b.get(result);
-			BSONObject o = BSON.decode(result);
-			d.object(JSON.serialize(o));
-		}
+        if (b != null && b.hasArray()) {
+            byte[] result = new byte[b.remaining()];
+            b.get(result);
+            BSONObject o = BSON.decode(result);
+            d.object(JSON.serialize(o));
+        }
 
-		d.setCreatedAt(row.getDate(Columns.CREATED_AT));
-		d.setUpdatedAt(row.getDate(Columns.UPDATED_AT));
-		return d;
+        d.setCreatedAt(row.getDate(Columns.CREATED_AT));
+        d.setUpdatedAt(row.getDate(Columns.UPDATED_AT));
+        return d;
     }
 
-	private class DocumentEventFactory
-	implements EventFactory<Document>
-	{
-		@Override
-        public Object newCreatedEvent(Document object)
-        {
-	        return new DocumentCreatedEvent(object);
+    private class DocumentEventFactory
+            implements EventFactory<Document> {
+
+        @Override
+        public Object newCreatedEvent(Document object) {
+            return new DocumentCreatedEvent(object);
         }
 
-		@Override
-        public Object newUpdatedEvent(Document object)
-        {
-	        return new DocumentUpdatedEvent(object);
+        @Override
+        public Object newUpdatedEvent(Document object) {
+            return new DocumentUpdatedEvent(object);
         }
 
-		@Override
-        public Object newDeletedEvent(Document object)
-        {
-	        return new DocumentDeletedEvent(object);
+        @Override
+        public Object newDeletedEvent(Document object) {
+            return new DocumentDeletedEvent(object);
         }
-	}
+    }
 }
