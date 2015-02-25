c4b2a5a67ba46ce28a1674ea3a1fcc5bd6a4d304
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-02-25 10:59:14 -0700

    lots of work with query parsing

--- driver/src/main/java/com/strategicgains/docussandra/service/QueryService.java
+++ driver/src/main/java/com/strategicgains/docussandra/service/QueryService.java
@@ -1,10 +1,16 @@
 package com.strategicgains.docussandra.service;
 
+import com.strategicgains.docussandra.Utils;
 import com.strategicgains.docussandra.domain.Document;
 import com.strategicgains.docussandra.domain.Index;
+import com.strategicgains.docussandra.domain.ParsedQuery;
 import com.strategicgains.docussandra.domain.Query;
+import com.strategicgains.docussandra.domain.WhereClause;
+import com.strategicgains.docussandra.exception.FieldNotIndexedException;
 import com.strategicgains.docussandra.persistence.IndexRepository;
 import com.strategicgains.docussandra.persistence.QueryDao;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class QueryService {
@@ -17,31 +23,83 @@ public class QueryService {
     }
 
     public List<Document> query(String db, Query toQuery) {
-        String table = validateQueryAndDetermineTable(db, toQuery);
-        if (table == null) {
-            //TODO: throw a new FieldNotIndexedException; include the bad field somehow
-            throw new RuntimeException("Field not indexed; query not possible");
-        } else {
-            return queries.doQuery(db, toQuery, table);
-        }
+        ParsedQuery parsedQuery = parseQuery(db, toQuery);//note: throws a runtime exception
+            return queries.doQuery(db, parsedQuery);
+        
     }
 
-    /**
-     * Parses a query to determine if it is valid and which table we need to
-     * query on. Will return null if the query is not valid.
-     *
-     * @param db Database that the query will run against
-     * @param toValidate Query to be validated.
-     * @return A string of the table name that the query will need to execute
-     * against. Null if the query is not valid.
-     */
-    public String validateQueryAndDetermineTable(String db, Query toValidate) {
+//    /**
+//     * Parses a query to determine if it is valid and which table we need to
+//     * query on. Will return null if the query is not valid.
+//     *
+//     * @param db Database that the query will run against
+//     * @param toValidate Query to be validated.
+//     * @return A string of the table name that the query will need to execute
+//     * against. Null if the query is not valid.
+//     */
+//    public ParsedQuery validateQueryAndDetermineTable(String db, Query toValidate) {
+//        //determine if the query is valid; in other words is it searching on valid fields that we have indexed
+//        IndexRepository indexRepo = new IndexRepository(queries.getSession());
+//        List<Index> indices = indexRepo.readAll(db, toValidate.getTable());
+//
+//        //determine which iTable(s) we need to query on -- the field could be in more than one iTable, so which one do we pick? the one with the least number of additional fields?
+//        return null;
+//    }
+    
+    //TODO: consider caching the results of this method; could be expensive and frequent
+    public ParsedQuery parseQuery(String db, Query toParse) throws FieldNotIndexedException {        
+        //let's parse the where clause so we know what we are actually searching for
+        WhereClause where = new WhereClause(toParse.getWhere());
         //determine if the query is valid; in other words is it searching on valid fields that we have indexed
+        List<String> fieldsToQueryOn = where.getFields();
         IndexRepository indexRepo = new IndexRepository(queries.getSession());
-        List<Index> indices = indexRepo.readAll(db, toValidate.getTable());
+        List<Index> indices = indexRepo.readAll(db, toParse.getTable());
+        Index indexToUse = null;
+        for (Index index : indices) {
+            if (equalLists(index.fields(), fieldsToQueryOn)) {
+                indexToUse = index;//we have a perfect match; the index matches the query exactly
+                break;
+            }
+        }
+        if (indexToUse == null) {//whoops, no perfect match, let try for a partial match (ie, the index has more fields than the query)
+            for (Index index : indices) {
+                //make a copy of the fieldsToQueryOn so we don't mutate the orginal
+                ArrayList<String> fieldsToQueryOnCopy = new ArrayList<>(fieldsToQueryOn);
+                ArrayList<String> indexFields = new ArrayList<>(index.fields());//make a copy here too
+                fieldsToQueryOnCopy.removeAll(indexFields);//we remove all the fields we have, from the fields we want
+                //if there are not any fields left in fields we want
+                if(fieldsToQueryOn.isEmpty()){
+                   //we have an index that will work (even though we have extra fields in it)
+                   indexToUse = index;
+                   break;
+                }
+            }
+        }
+        if(indexToUse == null){
+            throw new FieldNotIndexedException(fieldsToQueryOn);
+        }
+        ParsedQuery toReturn = new ParsedQuery(toParse, where, Utils.calculateITableName(indexToUse));        
+        return toReturn;
+    }
 
-        //determine which iTable(s) we need to query on -- the field could be in more than one iTable, so which one do we pick? the one with the least number of additional fields?
-        return null;
+    public boolean equalLists(List<String> one, List<String> two) {
+        if (one == null && two == null) {
+            return true;
+        }
+
+        if ((one == null && two != null)
+                || one != null && two == null
+                || one.size() != two.size()) {
+            return false;
+        }
+
+        //to avoid messing the order of the lists we will use a copy
+        ArrayList<String> oneCopy = new ArrayList<>(one);
+        ArrayList<String> twoCopy = new ArrayList<>(two);
+
+        Collections.sort(oneCopy);
+        Collections.sort(twoCopy);
+        return one.equals(twoCopy);
     }
 
 //
