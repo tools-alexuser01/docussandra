99af3d5c3ae5c079c7f4cb7e377b54ecce482155
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-04 11:55:10 -0700

    Query headers are more accurate now, but not entirely correct

--- api/src/main/java/com/strategicgains/docussandra/controller/QueryController.java
+++ api/src/main/java/com/strategicgains/docussandra/controller/QueryController.java
@@ -11,6 +11,8 @@ import com.strategicgains.hyperexpress.builder.UrlBuilder;
 import java.util.List;
 import org.restexpress.common.query.QueryRange;
 import org.restexpress.query.QueryRanges;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This is the 'controller' layer, where HTTP details are converted to domain
@@ -22,18 +24,19 @@ import org.restexpress.query.QueryRanges;
  */
 public class QueryController
 {
-    
+
     private static final UrlBuilder LOCATION_BUILDER = new UrlBuilder();
     private static final int DEFAULT_LIMIT = 20;
-    
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private QueryService service;
-    
+
     public QueryController(QueryService queryService)
     {
         super();
         this.service = queryService;
     }
-    
+
     public List<Document> query(Request request, Response response)
     {
         String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
@@ -42,7 +45,7 @@ public class QueryController
         toQuery.setTable(table);//change of plans, no longer getting it from the query object, but from the URL instead
 
         QueryRange range = QueryRanges.parseFrom(request);
-        
+
         int limit = DEFAULT_LIMIT;//TODO: enforce maximum limit
         long offset = 0;
         if (range != null)
@@ -63,7 +66,18 @@ public class QueryController
             }
         }
         List<Document> queryResponse = service.query(database, toQuery, limit, offset);
-        response.setCollectionResponse(range, queryResponse.size(), queryResponse.size() + offset);//TODO: double check this
+        //TODO: Headers are not entirely correct here: https://github.com/tfredrich/docussandra/issues/50
+        if(queryResponse.isEmpty()){
+            // ?? response.setCollectionResponse(range, queryResponse.size(), queryResponse.size());//https://github.com/tfredrich/docussandra/issues/50
+        } 
+        else if (queryResponse.size() >= limit)
+        {
+            response.setCollectionResponse(range, queryResponse.size(), queryResponse.size() + 1);//https://github.com/tfredrich/docussandra/issues/50
+        } else
+        {
+            response.setCollectionResponse(range, queryResponse.size(), queryResponse.size() + offset);//https://github.com/tfredrich/docussandra/issues/50
+        }
+        logger.debug("Query: " + toQuery.toString() + " returned " + queryResponse.size() + " documents.");
         //Document document = documents.read(database, table, new Identifier(database, table, UuidConverter.parse(id)));
         // enrich the entity with links, etc. here...
         //TODO: come back to thisHyperExpress.bind(Constants.Url.DOCUMENT_ID, document.getUuid().toString());
