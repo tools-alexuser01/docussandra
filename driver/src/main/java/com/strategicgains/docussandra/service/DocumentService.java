09e917edcf492421a865e8d9d89a81482ece6e20
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-05 13:08:06 -0700

    no changes

--- driver/src/main/java/com/strategicgains/docussandra/service/DocumentService.java
+++ driver/src/main/java/com/strategicgains/docussandra/service/DocumentService.java
@@ -9,31 +9,31 @@ import com.strategicgains.syntaxe.ValidationEngine;
 
 public class DocumentService
 {
-	private TableRepository tables;
-	private DocumentRepository docs;
-	
-	public DocumentService(TableRepository databaseRepository, DocumentRepository documentRepository)
-	{
-		super();
-		this.docs = documentRepository;
-		this.tables = databaseRepository;
-	}
+    private TableRepository tables;
+    private DocumentRepository docs;
 
-	public Document create(String database, String table, String json)
-	{
-		verifyTable(database, table);
-			
-		Document doc = new Document();
-		doc.table(database, table);
-		doc.object(json);
-		ValidationEngine.validateAndThrow(doc);
-		return docs.create(doc);
-	}
-
-	public Document read(String database, String table, Identifier id)
+    public DocumentService(TableRepository databaseRepository, DocumentRepository documentRepository)
     {
-		verifyTable(database, table);
-		return docs.read(id);
+        super();
+        this.docs = documentRepository;
+        this.tables = databaseRepository;
+    }
+
+    public Document create(String database, String table, String json)
+    {
+        verifyTable(database, table);
+
+        Document doc = new Document();
+        doc.table(database, table);
+        doc.object(json);
+        ValidationEngine.validateAndThrow(doc);
+        return docs.create(doc);
+    }
+
+    public Document read(String database, String table, Identifier id)
+    {
+        verifyTable(database, table);
+        return docs.read(id);
     }
 
 //	public List<Document> readAll(String database, String table)
@@ -46,26 +46,26 @@ public class DocumentService
 //	{
 //		return docs.countAll(database, table);
 //	}
-
-	public void update(Document entity)
+    
+    public void update(Document entity)
     {
-		ValidationEngine.validateAndThrow(entity);
-		docs.update(entity);
+        ValidationEngine.validateAndThrow(entity);
+        docs.update(entity);
     }
 
-	public void delete(String database, String table, Identifier id)
+    public void delete(String database, String table, Identifier id)
     {
-		verifyTable(database, table);
-		docs.delete(id);
+        verifyTable(database, table);
+        docs.delete(id);
     }
 
-	private void verifyTable(String database, String table)
+    private void verifyTable(String database, String table)
     {
-	    Identifier tableId = new Identifier(database, table);
+        Identifier tableId = new Identifier(database, table);
 
-		if (!tables.exists(tableId))
-		{
-			throw new ItemNotFoundException("Table not found: " + tableId.toString());
-		}
+        if (!tables.exists(tableId))
+        {
+            throw new ItemNotFoundException("Table not found: " + tableId.toString());
+        }
     }
 }
