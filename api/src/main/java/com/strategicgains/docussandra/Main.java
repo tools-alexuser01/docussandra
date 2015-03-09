90a0cb89a1dc4b164e328736145ea394f618348c
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-09 15:34:27 -0600

    code format only

--- api/src/main/java/com/strategicgains/docussandra/Main.java
+++ api/src/main/java/com/strategicgains/docussandra/Main.java
@@ -33,97 +33,95 @@ import com.strategicgains.syntaxe.ValidationException;
 
 public class Main
 {
-	private static final String SERVICE_NAME = "Docussandra API";
-	private static final Logger LOG = LoggerFactory.getLogger(SERVICE_NAME);
 
-
-	public static void main(String[] args) throws Exception
-	{
-		RestExpress server = initializeServer(args);
-                LOG.info("Server started up!");
-		server.awaitShutdown();
-	}
-
-	public static RestExpress initializeServer(String[] args) throws IOException
-	{
-		RestExpress.setSerializationProvider(new SerializationProvider());
-		Identifiers.UUID.useShortUUID(true);
-
-		Configuration config = loadEnvironment(args);
-		RestExpress server = new RestExpress()
-				.setName(config.getProjectName(SERVICE_NAME))
-				.setBaseUrl(config.getBaseUrl())
-				.setExecutorThreadCount(config.getExecutorThreadPoolSize())
-				.addMessageObserver(new SimpleConsoleLogMessageObserver());
-
-		new VersionPlugin(config.getProjectVersion())
-			.register(server);
-
-		Routes.define(config, server);
-		Relationships.define(server);
-		configurePlugins(config, server);
-		mapExceptions(server);
-		server.bind(config.getPort());
-		return server;
-    }
-
-	private static void configurePlugins(Configuration config, RestExpress server)
-	{
-		configureMetrics(config, server);
-
-		new HyperExpressPlugin(Linkable.class)
-			.register(server);
-	}
-
-	private static void configureMetrics(Configuration config, RestExpress server)
+    private static final String SERVICE_NAME = "Docussandra API";
+    private static final Logger LOG = LoggerFactory.getLogger(SERVICE_NAME);
+    
+    public static void main(String[] args) throws Exception
     {
-		MetricsConfig mc = config.getMetricsConfig();
-
-	    if (mc.isEnabled())
-		{
-	    	MetricRegistry registry = new MetricRegistry();
-			new MetricsPlugin(registry)
-				.register(server);
-
-			if (mc.isGraphiteEnabled())
-			{
-				final Graphite graphite = new Graphite(new InetSocketAddress(mc.getGraphiteHost(), mc.getGraphitePort()));
-				final GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
-					.prefixedWith(mc.getPrefix())
-					.convertRatesTo(TimeUnit.SECONDS)
-					.convertDurationsTo(TimeUnit.MILLISECONDS)
-					.filter(MetricFilter.ALL)
-					.build(graphite);
-				reporter.start(mc.getPublishSeconds(), TimeUnit.SECONDS);
-			}
-			else
-			{
-				LOG.warn("*** Graphite Metrics Publishing is Disabled ***");
-			}
-		}
-		else
-		{
-			LOG.warn("*** Metrics Generation is Disabled ***");
-		}
+        RestExpress server = initializeServer(args);
+        LOG.info("Server started up!");
+        server.awaitShutdown();
     }
-
+    
+    public static RestExpress initializeServer(String[] args) throws IOException
+    {
+        RestExpress.setSerializationProvider(new SerializationProvider());
+        Identifiers.UUID.useShortUUID(true);
+        
+        Configuration config = loadEnvironment(args);
+        RestExpress server = new RestExpress()
+                .setName(config.getProjectName(SERVICE_NAME))
+                .setBaseUrl(config.getBaseUrl())
+                .setExecutorThreadCount(config.getExecutorThreadPoolSize())
+                .addMessageObserver(new SimpleConsoleLogMessageObserver());
+        
+        new VersionPlugin(config.getProjectVersion())
+                .register(server);
+        
+        Routes.define(config, server);
+        Relationships.define(server);
+        configurePlugins(config, server);
+        mapExceptions(server);
+        server.bind(config.getPort());
+        return server;
+    }
+    
+    private static void configurePlugins(Configuration config, RestExpress server)
+    {
+        configureMetrics(config, server);
+        
+        new HyperExpressPlugin(Linkable.class)
+                .register(server);
+    }
+    
+    private static void configureMetrics(Configuration config, RestExpress server)
+    {
+        MetricsConfig mc = config.getMetricsConfig();
+        
+        if (mc.isEnabled())
+        {
+            MetricRegistry registry = new MetricRegistry();
+            new MetricsPlugin(registry)
+                    .register(server);
+            
+            if (mc.isGraphiteEnabled())
+            {
+                final Graphite graphite = new Graphite(new InetSocketAddress(mc.getGraphiteHost(), mc.getGraphitePort()));
+                final GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+                        .prefixedWith(mc.getPrefix())
+                        .convertRatesTo(TimeUnit.SECONDS)
+                        .convertDurationsTo(TimeUnit.MILLISECONDS)
+                        .filter(MetricFilter.ALL)
+                        .build(graphite);
+                reporter.start(mc.getPublishSeconds(), TimeUnit.SECONDS);
+            } else
+            {
+                LOG.warn("*** Graphite Metrics Publishing is Disabled ***");
+            }
+        } else
+        {
+            LOG.warn("*** Metrics Generation is Disabled ***");
+        }
+    }
+    
     private static void mapExceptions(RestExpress server)
     {
-    	server
-	    	.mapException(ItemNotFoundException.class, NotFoundException.class)
-	    	.mapException(DuplicateItemException.class, ConflictException.class)
-	    	.mapException(ValidationException.class, BadRequestException.class)
-	    	.mapException(InvalidObjectIdException.class, BadRequestException.class);
+        server
+                .mapException(ItemNotFoundException.class, NotFoundException.class)
+                .mapException(DuplicateItemException.class, ConflictException.class)
+                .mapException(ValidationException.class, BadRequestException.class)
+                .mapException(InvalidObjectIdException.class, BadRequestException.class);
     }
-
-	private static Configuration loadEnvironment(String[] args)
-    throws FileNotFoundException, IOException
+    
+    private static Configuration loadEnvironment(String[] args)
+            throws FileNotFoundException, IOException
     {
-	    if (args.length > 0)
-		{
-			return Environment.from(args[0], Configuration.class);
-		}
-
-	    return Environment.fromDefault(Configuration.class);
+        if (args.length > 0)
+        {
+            return Environment.from(args[0], Configuration.class);
+        }
+        
+        return Environment.fromDefault(Configuration.class);
     }
 }
