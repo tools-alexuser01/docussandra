c6ad23db1837effe511452f9ffa8571dc0893adc
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-11 10:15:30 -0600

    added WW sql file

---
+++ api/config/ww.sql
@@ -0,0 +1,3 @@
+INSERT OR REPLACE INTO config (serverFunction, name, value) values ('A','port','8081');
+INSERT OR REPLACE INTO config (serverFunction, name, value) values ('A','executor.threadPool.size','20');
+INSERT OR REPLACE INTO config (serverFunction, name, value) values ('A','cassandra.keyspace','docussandra');
