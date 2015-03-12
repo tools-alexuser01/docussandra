f15dddf3c7777b7350dba9536edbdaba14312220
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-12 13:21:27 -0600

    #64 and #65

--- driver/src/main/java/com/strategicgains/docussandra/handler/IndexDeletedHandler.java
+++ driver/src/main/java/com/strategicgains/docussandra/handler/IndexDeletedHandler.java
@@ -1,30 +1,46 @@
 package com.strategicgains.docussandra.handler;
 
+import com.datastax.driver.core.Session;
+import com.strategicgains.docussandra.domain.Index;
 import com.strategicgains.docussandra.event.IndexDeletedEvent;
+import com.strategicgains.docussandra.persistence.ITableRepository;
 import com.strategicgains.eventing.EventHandler;
 
 /**
- * 
+ *
  * @author toddf
  * @since Nov 19, 2014
  */
 public class IndexDeletedHandler
-implements EventHandler
+        implements EventHandler
 {
-	@Override
-	public void handle(Object event) throws Exception
-	{
-		handle((IndexDeletedEvent) event);
-	}
 
-	public void handle(IndexDeletedEvent event)
-	{
-		//TODO: remove the index table(s).  
-	}
+    private Session dbSession;
 
-	@Override
-	public boolean handles(Class<?> eventClass)
-	{
-		return IndexDeletedEvent.class.isAssignableFrom(eventClass);
-	}
+    public IndexDeletedHandler(Session dbSession)
+    {
+        this.dbSession = dbSession;
+    }
+
+    @Override
+    public void handle(Object event) throws Exception
+    {
+        handle((IndexDeletedEvent) event);
+    }
+
+    public void handle(IndexDeletedEvent event)
+    {
+        ITableRepository itr = new ITableRepository(dbSession);
+        Index toDelete = event.data;
+        if(itr.iTableExists(toDelete)){
+            itr.deleteITable(toDelete);
+        }
+        
+    }
+
+    @Override
+    public boolean handles(Class<?> eventClass)
+    {
+        return IndexDeletedEvent.class.isAssignableFrom(eventClass);
+    }
 }
