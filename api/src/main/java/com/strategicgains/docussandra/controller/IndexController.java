c72a323af5eb438dc99c2294b807c404916cf283
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-03-12 15:40:36 -0600

    added some error logging

--- api/src/main/java/com/strategicgains/docussandra/controller/IndexController.java
+++ api/src/main/java/com/strategicgains/docussandra/controller/IndexController.java
@@ -16,111 +16,123 @@ import com.strategicgains.hyperexpress.builder.TokenBinder;
 import com.strategicgains.hyperexpress.builder.TokenResolver;
 import com.strategicgains.hyperexpress.builder.UrlBuilder;
 import com.strategicgains.repoexpress.domain.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * This is the 'controller' layer, where HTTP details are converted to domain concepts and passed to the service layer.
- * Then service layer response information is enhanced with HTTP details, if applicable, for the response.
+ * This is the 'controller' layer, where HTTP details are converted to domain
+ * concepts and passed to the service layer. Then service layer response
+ * information is enhanced with HTTP details, if applicable, for the response.
  * <p/>
- * This controller demonstrates how to process a Cassandra entity that is identified by a single, primary row key such
- * as a UUID.
+ * This controller demonstrates how to process a Cassandra entity that is
+ * identified by a single, primary row key such as a UUID.
  */
 public class IndexController
 {
-	private static final UrlBuilder LOCATION_BUILDER = new UrlBuilder();
 
-	private IndexService indexes;
-	
-	public IndexController(IndexService indexService)
-	{
-		super();
-		this.indexes = indexService;
-	}
+    private static final UrlBuilder LOCATION_BUILDER = new UrlBuilder();
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
 
-	public Index create(Request request, Response response)
-	{
-		String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
-		String table = request.getHeader(Constants.Url.TABLE, "No table provided");
-		String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
-		Index entity = request.getBodyAs(Index.class, "Resource details not provided");
-		Table t = new Table();
-		t.database(database);
-		t.name(table);
-		entity.table(t);
-		entity.name(name);
-		Index saved = indexes.create(entity);
+    private IndexService indexes;
 
-		// Construct the response for create...
-		response.setResponseCreated();
+    public IndexController(IndexService indexService)
+    {
+        super();
+        this.indexes = indexService;
+    }
 
-		// enrich the resource with links, etc. here...
-		TokenResolver resolver = HyperExpress.bind(Constants.Url.TABLE, saved.tableName())
-			.bind(Constants.Url.DATABASE, saved.databaseName())
-			.bind(Constants.Url.INDEX, saved.name());
+    public Index create(Request request, Response response)
+    {
+        String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
+        String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+        String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
+        Index entity = request.getBodyAs(Index.class, "Resource details not provided");
+        Table t = new Table();
+        t.database(database);
+        t.name(table);
+        entity.table(t);
+        entity.name(name);
+        Index saved;
+        try
+        {
+            saved = indexes.create(entity);
+        } catch (Exception e)
+        {
+            LOGGER.error("Could not save index", e);
+            throw e;
+        }
+        // Construct the response for create...
+        response.setResponseCreated();
 
-		// Include the Location header...
-		String locationPattern = request.getNamedUrl(HttpMethod.GET, Constants.Routes.INDEX);
-		response.addLocationHeader(LOCATION_BUILDER.build(locationPattern, resolver));
+        // enrich the resource with links, etc. here...
+        TokenResolver resolver = HyperExpress.bind(Constants.Url.TABLE, saved.tableName())
+                .bind(Constants.Url.DATABASE, saved.databaseName())
+                .bind(Constants.Url.INDEX, saved.name());
 
-		// Return the newly-created resource...
-		return saved;
-	}
+        // Include the Location header...
+        String locationPattern = request.getNamedUrl(HttpMethod.GET, Constants.Routes.INDEX);
+        response.addLocationHeader(LOCATION_BUILDER.build(locationPattern, resolver));
 
-	public Index read(Request request, Response response)
-	{
-		String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
-		String table = request.getHeader(Constants.Url.TABLE, "No table provided");
-		String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
-		Index entity = indexes.read(new Identifier(database, table, name));
+        // Return the newly-created resource...
+        return saved;
+    }
 
-		// enrich the entity with links, etc. here...
-		HyperExpress.bind(Constants.Url.TABLE, entity.tableName())
-			.bind(Constants.Url.DATABASE, entity.databaseName())
-			.bind(Constants.Url.INDEX, entity.name());
+    public Index read(Request request, Response response)
+    {
+        String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
+        String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+        String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
+        Index entity = indexes.read(new Identifier(database, table, name));
 
-		return entity;
-	}
+        // enrich the entity with links, etc. here...
+        HyperExpress.bind(Constants.Url.TABLE, entity.tableName())
+                .bind(Constants.Url.DATABASE, entity.databaseName())
+                .bind(Constants.Url.INDEX, entity.name());
 
-	public List<Index> readAll(Request request, Response response)
-	{
-		String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
-		String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+        return entity;
+    }
 
-		HyperExpress.tokenBinder(new TokenBinder<Index>()
-		{
-			@Override
+    public List<Index> readAll(Request request, Response response)
+    {
+        String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
+        String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+
+        HyperExpress.tokenBinder(new TokenBinder<Index>()
+        {
+            @Override
             public void bind(Index object, TokenResolver resolver)
             {
-				resolver.bind(Constants.Url.TABLE, object.tableName())
-					.bind(Constants.Url.DATABASE, object.databaseName())
-					.bind(Constants.Url.INDEX, object.name());
+                resolver.bind(Constants.Url.TABLE, object.tableName())
+                        .bind(Constants.Url.DATABASE, object.databaseName())
+                        .bind(Constants.Url.INDEX, object.name());
 
             }
-		});
+        });
 
-		return indexes.readAll(database, table);
-	}
+        return indexes.readAll(database, table);
+    }
 
-	public void update(Request request, Response response)
-	{
-		String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
-		String table = request.getHeader(Constants.Url.TABLE, "No table provided");
-		String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
-		Index entity = request.getBodyAs(Index.class, "Resource details not provided");
-		Table t = new Table();
-		t.database(database);
-		t.name(table);
-		entity.table(t);
-		entity.name(name);
-		indexes.update(entity);
-		response.setResponseNoContent();
-	}
+    public void update(Request request, Response response)
+    {
+        String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
+        String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+        String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
+        Index entity = request.getBodyAs(Index.class, "Resource details not provided");
+        Table t = new Table();
+        t.database(database);
+        t.name(table);
+        entity.table(t);
+        entity.name(name);
+        indexes.update(entity);
+        response.setResponseNoContent();
+    }
 
-	public void delete(Request request, Response response)
-	{
-		String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
-		String table = request.getHeader(Constants.Url.TABLE, "No table provided");
-		String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
-		indexes.delete(new Identifier(database, table, name));
-		response.setResponseNoContent();
-	}
+    public void delete(Request request, Response response)
+    {
+        String database = request.getHeader(Constants.Url.DATABASE, "No database provided");
+        String table = request.getHeader(Constants.Url.TABLE, "No table provided");
+        String name = request.getHeader(Constants.Url.INDEX, "No index name provided");
+        indexes.delete(new Identifier(database, table, name));
+        response.setResponseNoContent();
+    }
 }
