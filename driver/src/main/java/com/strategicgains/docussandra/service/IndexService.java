39ab9ada4b7437498a72a56eea1d2c9cc2ac2159
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-04-10 09:25:46 -0600

    code format only

--- driver/src/main/java/com/strategicgains/docussandra/service/IndexService.java
+++ driver/src/main/java/com/strategicgains/docussandra/service/IndexService.java
@@ -3,6 +3,7 @@ package com.strategicgains.docussandra.service;
 import java.util.List;
 
 import com.strategicgains.docussandra.domain.Index;
+import com.strategicgains.docussandra.domain.IndexCreationStatus;
 import com.strategicgains.docussandra.persistence.IndexRepository;
 import com.strategicgains.docussandra.persistence.TableRepository;
 import com.strategicgains.repoexpress.domain.Identifier;
@@ -13,61 +14,62 @@ import org.slf4j.LoggerFactory;
 
 public class IndexService
 {
-	private TableRepository tables;
-	private IndexRepository indexes;
-        
-        private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	public IndexService(TableRepository tableRepository, IndexRepository indexRepository)
-	{
-		super();
-		this.indexes = indexRepository;
-		this.tables = tableRepository;
-	}
 
-	public Index create(Index index)
-	{
-		verifyTable(index.databaseName(), index.tableName());
-		ValidationEngine.validateAndThrow(index);
-                logger.debug("Creating index: " + index.toString());
-		return indexes.create(index);
-	}
+    private TableRepository tables;
+    private IndexRepository indexes;
 
-	public Index read(Identifier identifier)
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public IndexService(TableRepository tableRepository, IndexRepository indexRepository)
     {
-		return indexes.read(identifier);
+        super();
+        this.indexes = indexRepository;
+        this.tables = tableRepository;
     }
 
-	public void update(Index index)
+    public Index create(Index index)
     {
-		ValidationEngine.validateAndThrow(index);
-                logger.debug("Updating index: " + index.toString());
-		indexes.update(index);
+        verifyTable(index.databaseName(), index.tableName());
+        ValidationEngine.validateAndThrow(index);
+        logger.debug("Creating index: " + index.toString());
+        return indexes.create(index);
     }
 
-	public void delete(Identifier identifier)
+    public Index read(Identifier identifier)
     {
-                logger.debug("Deleting index: " + identifier.toString());
-		indexes.delete(identifier);
+        return indexes.read(identifier);
     }
 
-	public List<Index> readAll(String namespace, String collection)
+    public void update(Index index)
     {
-	    return indexes.readAllCached(namespace, collection);
+        ValidationEngine.validateAndThrow(index);
+        logger.debug("Updating index: " + index.toString());
+        indexes.update(index);
     }
 
-	public long count(String namespace, String collection)
+    public void delete(Identifier identifier)
     {
-	    return indexes.countAll(namespace, collection);
+        logger.debug("Deleting index: " + identifier.toString());
+        indexes.delete(identifier);
     }
 
-	private void verifyTable(String database, String table)
+    public List<Index> readAll(String namespace, String collection)
     {
-	    Identifier tableId = new Identifier(database, table);
+        return indexes.readAllCached(namespace, collection);
+    }
 
-		if (!tables.exists(tableId))
-		{
-			throw new ItemNotFoundException("Table not found: " + tableId.toString());
-		}
+    public long count(String namespace, String collection)
+    {
+        return indexes.countAll(namespace, collection);
+    }
+
+    private void verifyTable(String database, String table)
+    {
+        Identifier tableId = new Identifier(database, table);
+
+        if (!tables.exists(tableId))
+        {
+            throw new ItemNotFoundException("Table not found: " + tableId.toString());
+        }
     }
 }
