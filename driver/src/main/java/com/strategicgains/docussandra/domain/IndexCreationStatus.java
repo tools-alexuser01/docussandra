688859747ce19ee64c19e0e104a258feae215be9
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-04-10 08:56:11 -0600

    #4 -- more status object work

--- driver/src/main/java/com/strategicgains/docussandra/domain/IndexCreationStatus.java
+++ driver/src/main/java/com/strategicgains/docussandra/domain/IndexCreationStatus.java
@@ -15,37 +15,230 @@
  */
 package com.strategicgains.docussandra.domain;
 
+import com.strategicgains.repoexpress.domain.Identifier;
+import com.strategicgains.repoexpress.domain.UuidIdentifiable;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * POJO that contains the current status of an indexing action.
+ *
  * @author udeyoje
  */
-public class IndexCreationStatus
+public class IndexCreationStatus implements UuidIdentifiable
 {
+
+    /**
+     * UUID for this object.
+     */
+    private UUID id;
+
     /**
      * The date that this request to make an index was issued.
      */
     private Date dateStarted;
-        
+
+    /**
+     * The last time this status was updated.
+     */
+    private Date statusLastUpdatedAt;
+
     /**
      * Estimated time to completion of this index.
      */
     private long eta;//likely a pipe dream, but it would be sweet if this worked
-    
+
     /**
      * The requested index that is being created.
      */
     private Index index;
+
+    /**
+     * Total number of records that this index will have when complete.
+     */
+    private long totalRecords;
+
+    /**
+     * Number of records that have been indexed.
+     */
+    private long recordsCompleted;
+
+    /**
+     * Default constructor.
+     */
+    public IndexCreationStatus()
+    {
+    }
+
+    /**
+     * Constructor.
+     * @param id
+     * @param dateStarted
+     * @param statusLastUpdatedAt
+     * @param index
+     * @param totalRecords
+     * @param recordsCompleted 
+     */
+    public IndexCreationStatus(UUID id, Date dateStarted, Date statusLastUpdatedAt, Index index, long totalRecords, long recordsCompleted)
+    {
+        this.id = id;
+        this.dateStarted = dateStarted;
+        this.statusLastUpdatedAt = statusLastUpdatedAt;
+        this.index = index;
+        this.totalRecords = totalRecords;
+        this.recordsCompleted = recordsCompleted;
+    }
+
     
     
     
-//    public UUID getId(){
-//        return index.getId().
-//    }
-    
-    
-    public boolean isDone(){
-        return index.isIsActive();
+    public boolean isDone()
+    {
+        return getIndex().isIsActive();
+    }
+
+    /**
+     * Calculates out what percent complete this operation is. If more records
+     * get added during the operation, the percent complete could decrease
+     * instead of climb.
+     *
+     * @return The current percent complete of this index creation operation.
+     */
+    public double calculatePercentComplete()
+    {
+        return (double) ((double) getRecordsCompleted() / (double) getTotalRecords()) * 100d;
+    }
+
+    @Override
+    public UUID getUuid()
+    {
+        return id;
+    }
+
+    @Override
+    public void setUuid(UUID id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public Identifier getId()
+    {
+        return new Identifier(getIndex().databaseName(), getIndex().tableName(), getIndex().name(), id);
+    }
+
+    @Override
+    public void setId(Identifier id)
+    {
+        // Do nothing. Throw.
+        throw new UnsupportedOperationException("This is not a valid call for this object.");
+    }
+
+    /**
+     * The date that this request to make an index was issued.
+     * @return the dateStarted
+     */
+    public Date getDateStarted()
+    {
+        return dateStarted;
+    }
+
+    /**
+     * The date that this request to make an index was issued.
+     * @param dateStarted the dateStarted to set
+     */
+    public void setDateStarted(Date dateStarted)
+    {
+        this.dateStarted = dateStarted;
+    }
+
+    /**
+     * The last time this status was updated.
+     * @return the statusLastUpdatedAt
+     */
+    public Date getStatusLastUpdatedAt()
+    {
+        return statusLastUpdatedAt;
+    }
+
+    /**
+     * The last time this status was updated.
+     * @param statusLastUpdatedAt the statusLastUpdatedAt to set
+     */
+    public void setStatusLastUpdatedAt(Date statusLastUpdatedAt)
+    {
+        this.statusLastUpdatedAt = statusLastUpdatedAt;
+    }
+
+    /**
+     * Estimated time to completion of this index.
+     * @return the eta
+     */
+    public long getEta()
+    {
+        return eta;
+    }
+
+    /**
+     * Estimated time to completion of this index.
+     * @param eta the eta to set
+     */
+    public void setEta(long eta)
+    {
+        this.eta = eta;
+    }
+
+    /**
+     * The requested index that is being created.
+     * @return the index
+     */
+    public Index getIndex()
+    {
+        return index;
+    }
+
+    /**
+     * The requested index that is being created.
+     * @param index the index to set
+     */
+    public void setIndex(Index index)
+    {
+        this.index = index;
+    }
+
+    /**
+     * Total number of records that this index will have when complete.
+     * @return the totalRecords
+     */
+    public long getTotalRecords()
+    {
+        return totalRecords;
+    }
+
+    /**
+     * Total number of records that this index will have when complete.
+     * @param totalRecords the totalRecords to set
+     */
+    public void setTotalRecords(long totalRecords)
+    {
+        this.totalRecords = totalRecords;
+    }
+
+    /**
+     * Number of records that have been indexed.
+     * @return the recordsCompleted
+     */
+    public long getRecordsCompleted()
+    {
+        return recordsCompleted;
+    }
+
+    /**
+     * Number of records that have been indexed.
+     * @param recordsCompleted the recordsCompleted to set
+     */
+    public void setRecordsCompleted(long recordsCompleted)
+    {
+        this.recordsCompleted = recordsCompleted;
     }
 }
