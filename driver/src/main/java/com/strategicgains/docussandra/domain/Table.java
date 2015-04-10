3f794003110de128b393bb8bf8d0c93168f18f52
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-04-10 10:33:35 -0600

    equals and hashcode

--- driver/src/main/java/com/strategicgains/docussandra/domain/Table.java
+++ driver/src/main/java/com/strategicgains/docussandra/domain/Table.java
@@ -8,6 +8,7 @@ import com.strategicgains.repoexpress.domain.Identifier;
 import com.strategicgains.syntaxe.annotation.ChildValidation;
 import com.strategicgains.syntaxe.annotation.RegexValidation;
 import com.strategicgains.syntaxe.annotation.Required;
+import java.util.Objects;
 
 public class Table
         extends AbstractTimestampedIdentifiable
@@ -135,4 +136,53 @@ public class Table
         }
         return sb.toString();
     }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.database);
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.description);
+        hash = 59 * hash + (int) (this.ttl ^ (this.ttl >>> 32));
+        hash = 59 * hash + (int) (this.deleteTtl ^ (this.deleteTtl >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final Table other = (Table) obj;
+        if (!Objects.equals(this.database, other.database))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description))
+        {
+            return false;
+        }
+        if (this.ttl != other.ttl)
+        {
+            return false;
+        }
+        if (this.deleteTtl != other.deleteTtl)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
