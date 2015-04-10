01553d3eafc74ec1f970b26c31db8ad398562370
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-04-10 12:07:07 -0600

    more work for #4

--- driver/src/main/java/com/strategicgains/docussandra/service/IndexService.java
+++ driver/src/main/java/com/strategicgains/docussandra/service/IndexService.java
@@ -1,14 +1,18 @@
 package com.strategicgains.docussandra.service;
 
+import com.strategicgains.docussandra.domain.Document;
 import java.util.List;
 
 import com.strategicgains.docussandra.domain.Index;
 import com.strategicgains.docussandra.domain.IndexCreationStatus;
 import com.strategicgains.docussandra.persistence.IndexRepository;
 import com.strategicgains.docussandra.persistence.TableRepository;
+import com.strategicgains.repoexpress.AbstractObservableRepository;
 import com.strategicgains.repoexpress.domain.Identifier;
+import com.strategicgains.repoexpress.event.UuidIdentityRepositoryObserver;
 import com.strategicgains.repoexpress.exception.ItemNotFoundException;
 import com.strategicgains.syntaxe.ValidationEngine;
+import java.util.Date;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +38,9 @@ public class IndexService
         ValidationEngine.validateAndThrow(index);
         logger.debug("Creating index: " + index.toString());
         Index created = indexes.create(index);
-        //long dataSize = tables.countAll(null);
+        long dataSize = tables.countTableSize(index.databaseName(), index.tableName());
+        Date now = new Date();
+        
         return created;
     }
     
@@ -49,16 +55,24 @@ public class IndexService
     {
         return indexes.read(identifier);
     }
-
-    public void update(Index index)
+    
+    public Index update(Index index)
     {
         ValidationEngine.validateAndThrow(index);
         logger.debug("Updating index: " + index.toString());
         indexes.update(index);
+        return index;
     }
 
     public void delete(Identifier identifier)
     {
+        logger.debug("Deleting index: " + identifier.toString());
+        indexes.delete(identifier);
+    }
+    
+    public void delete(Index index)
+    {
+        Identifier identifier = index.getId();
         logger.debug("Deleting index: " + identifier.toString());
         indexes.delete(identifier);
     }
