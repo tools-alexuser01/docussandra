fe1d5fc1bf42a5ba87d0a071ea077b573ca6ad1f
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-05-06 09:29:40 -0600

    upped timeout for index creation

--- integration_test/src/test/java/com/strategicgains/docussandra/controller/IndexControllerTest.java
+++ integration_test/src/test/java/com/strategicgains/docussandra/controller/IndexControllerTest.java
@@ -461,7 +461,7 @@ public class IndexControllerTest
                     break;
                 }
                 LOGGER.debug("Waiting for index to go active for: " + sw.getTime());
-                if (sw.getTime() >= 12000)
+                if (sw.getTime() >= 20000)
                 {
                     fail("Index took too long to create: " + sw.getTime() + " -- " + body);
                 }
