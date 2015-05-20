04bc36985c9a76c41ee4d9cce71315ce168364dc
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-05-20 08:46:50 -0600

    fixed imports only

--- cassandra/src/main/java/com/strategicgains/docussandra/persistence/impl/IndexRepositoryImpl.java
+++ cassandra/src/main/java/com/strategicgains/docussandra/persistence/impl/IndexRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.strategicgains.docussandra.persistence.impl;
 
-import com.strategicgains.docussandra.persistence.impl.ITableRepositoryImpl;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
