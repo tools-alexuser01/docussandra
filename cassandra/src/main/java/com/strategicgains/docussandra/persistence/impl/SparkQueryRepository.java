7a57f6b0ff459c8020ee4e81f05c25154fba3b4e
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-06-01 15:01:19 -0600

    some POC work on spark-sql with cassandra; committing for the record

---
+++ cassandra/src/main/java/com/strategicgains/docussandra/persistence/impl/SparkQueryRepository.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 udeyoje.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.strategicgains.docussandra.persistence.impl;
+
+import com.strategicgains.docussandra.domain.Document;
+import com.strategicgains.docussandra.domain.Query;
+import com.strategicgains.docussandra.domain.QueryResponseWrapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.api.java.JavaSchemaRDD;
+import org.apache.spark.sql.api.java.Row;
+import org.apache.spark.sql.cassandra.api.java.JavaCassandraSQLContext;
+
+
+/**
+ *
+ * @author udeyoje
+ */
+@Deprecated
+public class SparkQueryRepository
+{
+
+    public QueryResponseWrapper query(Query query)
+    {
+
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("SparkTest")
+                .set("spark.executor.memory", "1g")
+                .set("spark.cassandra.connection.host", "localhost")
+                .set("spark.cassandra.connection.native.port", "9042")
+                .set("spark.cassandra.connection.rpc.port", "9160");
+        JavaSparkContext context = new JavaSparkContext(sparkConf);
+        JavaCassandraSQLContext sqlContext = new JavaCassandraSQLContext(context);
+        sqlContext.sqlContext().setKeyspace("docussandra");
+        
+        String sql = "SELECT * FROM docussandra.players_players_table";// + query.getTable() + " WHERE " + query.getWhere();
+        JavaSchemaRDD rdd = sqlContext.sql(sql);
+        List<Row> rows = rdd.collect();
+        List<Document> docs = new ArrayList<>();
+        for(Row r : rows){
+            //toReturn.add(Utils.)
+        }
+        QueryResponseWrapper toReturn = new QueryResponseWrapper(docs, Long.MAX_VALUE);
+        return toReturn;
+    }
+}
---
+++ cassandra/src/test/java/com/strategicgains/docussandra/persistence/impl/SparkQueryRepositoryTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015 udeyoje.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.strategicgains.docussandra.persistence.impl;
+
+import com.strategicgains.docussandra.domain.Query;
+import com.strategicgains.docussandra.domain.QueryResponseWrapper;
+import com.strategicgains.docussandra.testhelper.Fixtures;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author udeyoje
+ */
+public class SparkQueryRepositoryTest
+{
+    
+    public SparkQueryRepositoryTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of query method, of class SparkQueryRepository.
+     */
+    @Test
+    @Ignore
+    public void testQuery()
+    {
+        System.out.println("query");
+        Query query = Fixtures.createTestQuery();
+        SparkQueryRepository instance = new SparkQueryRepository();
+        QueryResponseWrapper expResult = null;
+        QueryResponseWrapper result = instance.query(query);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
